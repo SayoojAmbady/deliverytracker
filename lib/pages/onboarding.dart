6afef6aed4ede9e4cacc1e706c051c87a66d5001
SayoojAmbady <sayoojsunil2255@gmail.com>
2025-10-25 12:28:28 +0530

    Initial Commit for Delivery Tracker

---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,47 @@
+
+import 'package:flutter/material.dart';
+
+import '../services/widget_support.dart';
+
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        margin: EdgeInsets.only(top: 20.0),
+        child: Column(
+          children: [Image.asset("assets/images/onboard.png"),
+            Text("Track your parcel\nfrom anywhere" , style: AppWidget.HeadlineTextfiledStyle(),
+              textAlign: TextAlign.center,),
+            SizedBox(height: 30),
+            Text("Check the Progress of\n your deliveries" , style: AppWidget.SimpleTextfiledStyle(),
+              textAlign: TextAlign.center,),
+            SizedBox(height: 30,),
+            Material(
+              elevation: 3.0,
+              borderRadius: BorderRadius.circular(40),
+              child: Container(
+                height: 60,
+                width: MediaQuery.of(context).size.width/1.7,
+                decoration: BoxDecoration(
+                    color: Color(0xfff8ae39),
+                    borderRadius: BorderRadius.circular(40)
+                ),
+                child: Center(child: Text("Track Now", style: AppWidget.WhiteTextfiledStyle(),),),
+              ),
+            )
+          ],
+        ),),
+    );
+
+  }
+}
---
+++ lib/services/widget_support.dart
@@ -0,0 +1,28 @@
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppWidget{
+  static TextStyle HeadlineTextfiledStyle(){
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 30.0,
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  static TextStyle SimpleTextfiledStyle(){
+    return TextStyle(
+      color: Colors.black38,
+      fontSize: 18.0,
+      fontWeight: FontWeight.w500,
+    );
+  }
+  static TextStyle WhiteTextfiledStyle(){
+    return TextStyle(
+      color: Colors.white,
+      fontSize: 20.0,fontWeight: FontWeight.bold,
+    );
+  }
+}
